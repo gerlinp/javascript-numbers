3b48b0437a1d24408082efba8b0a8299cbe96ce2
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-26 00:43:54 -0500

    Convert strings to numbers

--- javascript-numbers/1-Working with Numbers/3-Create a Program with Math/start/js/string-to-number.js
+++ javascript-numbers/1-Working with Numbers/3-Create a Program with Math/start/js/string-to-number.js
@@ -1,6 +1,8 @@
-const HTMLBadges = prompt('How many HTML badges do you have?');
-const CSSBadges = prompt('How many CSS badges do you have?');
+// const HTMLBadges = prompt('How many HTML badges do you have?');
+// const CSSBadges = prompt('How many CSS badges do you have?');
 
-const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
-console.log(totalBadges);
-console.log(typeof parseInt(HTMLBadges), typeof parseInt(CSSBadges));
+// const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+
+const pi = prompt('What is Pi?');
+console.log( +pi === 3.14 );
+

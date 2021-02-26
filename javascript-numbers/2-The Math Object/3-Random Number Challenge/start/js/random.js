f178a7546f35df7a3a5d9ee88cf305bdca021c36
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-26 02:18:26 -0500

    Random number challenge

--- javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
+++ javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
@@ -1,11 +1,19 @@
 // Collect input from a user
 
+let inputLow = prompt('Please provide your lowest number');
+let inputHigh = prompt('Please provide your highest number');
 
 // Convert the input to a number
+let lowNumber = parseInt(inputLow);
+let highNumber = parseInt(inputHigh);
 
 
-// Use Math.random() and the user's number to generate a random number
+if  (lowNumber && highNumber) {
+    // Use Math.random() and the user's number to generate a random number
+    const randomNumber = Math.floor( Math.random() * (highNumber - lowNumber + 1) ) + lowNumber;
 
-
-// Create a message displaying the random number
-
+    // Create a message displaying the random number
+    console.log(`${randomNumber} is a random number between ${lowNumber} and ${highNumber}`);
+} else {
+    console.log('You need to provide two numbers try again.')
+}
